86c3ae204ebbe359653958a790a3a9661eec6301
HoldYourWaffle <ravivanrooijen@live.nl>
2017-12-29 13:05:10 +0100

    Temporarily formatting to my own code style

--- src/main/java/me/ichun/mods/deathcounter/common/core/CommandDeathCounter.java
+++ src/main/java/me/ichun/mods/deathcounter/common/core/CommandDeathCounter.java
@@ -17,149 +17,84 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class CommandDeathCounter extends CommandBase
-{
+public class CommandDeathCounter extends CommandBase {
 	@Override
-	public String getName()
-	{
+	public String getName() {
 		return "dc";
 	}
-
+	
 	@Override
-	public String getUsage(ICommandSender par1ICommandSender)
-	{
+	public String getUsage(ICommandSender par1ICommandSender) {
 		return "/" + this.getName() + " (leaderboard/reset) [all/user]";
 	}
-
+	
 	@Override
-	public List<String> getAliases()
-	{
+	public List<String> getAliases() {
 		return Collections.singletonList("deathcounter");
 	}
-
+	
 	@Override
-	public void execute(MinecraftServer server, ICommandSender commandSender, String[] args) throws CommandException
-	{
-		if(args.length > 0)
-		{
-			if("leaderboard".toLowerCase().startsWith(args[0]))
-			{
-				if(args.length >= 2)
-				{
-					if(args[1].equalsIgnoreCase("all"))
-					{
-						broadcastLeaderboard(null);
-					}
-					else
-					{
-						broadcastLeaderboard(args[1]);
-					}
-				}
-				else
-				{
-					broadcastLeaderboard(commandSender.getName());
-				}
-			}
-			else if("reset".toLowerCase().startsWith(args[0]))
-			{
-				if(args.length >= 2)
-				{
-					if(args[1].equalsIgnoreCase("all"))
-					{
+	public void execute(MinecraftServer server, ICommandSender commandSender, String[] args) throws CommandException {
+		if (args.length > 0) {
+			if ("leaderboard".toLowerCase().startsWith(args[0])) {
+				if (args.length >= 2) {
+					if (args[1].equalsIgnoreCase("all")) broadcastLeaderboard(null);
+					else broadcastLeaderboard(args[1]);
+				} else broadcastLeaderboard(commandSender.getName());
+			} else if ("reset".toLowerCase().startsWith(args[0])) {
+				if (args.length >= 2) {
+					if (args[1].equalsIgnoreCase("all")) {
 						notifyCommandListener(commandSender, this, "dc.command.resetAll");
 						DeathCounter.console(commandSender.getName() + ": Resetting deaths for all players", Level.INFO);
-					}
-					else
-					{
-						if(DeathCounter.instance.clearDeath(args[1]))
-						{
+					} else {
+						if (DeathCounter.instance.clearDeath(args[1])) {
 							notifyCommandListener(commandSender, this, "dc.command.resetPlayer");
 							DeathCounter.console(commandSender.getName() + ": Resetting deaths for " + args[1], Level.INFO);
-						}
-						else
-						{
-							commandSender.sendMessage(new TextComponentTranslation("dc.command.noDeathsPlayer", args[1]));
-						}
+						} else commandSender.sendMessage(new TextComponentTranslation("dc.command.noDeathsPlayer", args[1]));
 					}
-				}
-				else
-				{
-					throw new WrongUsageException("/" + this.getName() + " reset [all/user]");
-				}
-			}
-			else
-			{
-				throw new WrongUsageException("/" + this.getName() + " (leaderboard/reset) [all/user]");
-			}
-		}
-		else
-		{
-			throw new WrongUsageException("/" + this.getName() + " (leaderboard/reset) [all/user]");
-		}
+				} else throw new WrongUsageException("/" + this.getName() + " reset [all/user]");
+			} else throw new WrongUsageException("/" + this.getName() + " (leaderboard/reset) [all/user]");
+		} else throw new WrongUsageException("/" + this.getName() + " (leaderboard/reset) [all/user]");
 	}
-
-	public static void broadcastLeaderboard(String s)
-	{
+	
+	public static void broadcastLeaderboard(String s) {
 		ArrayList<EntityPlayer> playersToNotify = new ArrayList<EntityPlayer>();
-		if(s != null)
-		{
-			if(!s.equals("Server"))
-			{
-				if(FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(s) == null)
-				{
-					return;
-				}
+		if (s != null) {
+			if (!s.equals("Server")) {
+				if (FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(s) == null) return;
 				playersToNotify.add(FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(s));
 			}
-		}
-		else
-		{
+		} else {
 			List<EntityPlayerMP> players = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers();
-			for(Object o : players)
-			{
-				EntityPlayer player = (EntityPlayer)o;
+			for (Object o : players) {
+				EntityPlayer player = (EntityPlayer) o;
 				playersToNotify.add(player);
 			}
 		}
-		if(s != null && s.equals("Server"))
-		{
-			if(DeathCounter.ranking.isEmpty())
-			{
-				DeathCounter.console("No deaths have occured on this server yet.", Level.INFO);
-			}
-			else
-			{
+		
+		if (s != null && s.equals("Server")) {
+			if (DeathCounter.ranking.isEmpty()) DeathCounter.console("No deaths have occured on this server yet.", Level.INFO);
+			else {
 				DeathCounter.console("Death Leaderboard:", Level.INFO);
-				for(int i = 1; i <= DeathCounter.leaderboardCount && i - 1 < DeathCounter.ranking.size(); i++)
-				{
+				for (int i = 1; i <= DeathCounter.leaderboardCount && i - 1 < DeathCounter.ranking.size(); i++) {
 					DeathCounter.console("   " + DeathCounter.instance.getDisplayedRank(DeathCounter.ranking.get(i - 1)) + "  " + DeathCounter.ranking.get(i - 1) + " (" + DeathCounter.instance.getDeathCount(DeathCounter.ranking.get(i - 1)) + (DeathCounter.instance.getDeathCount(DeathCounter.ranking.get(i - 1)) == 1 ? " Death)" : " Deaths)"), Level.INFO);
 				}
 			}
-		}
-		else
-		{
-			for(EntityPlayer player: playersToNotify)
-			{
-				if(DeathCounter.ranking.isEmpty())
-				{
-					player.sendStatusMessage(new TextComponentTranslation("dc.command.noDeaths"), false);
-				}
-				else
-				{
+		} else {
+			for (EntityPlayer player : playersToNotify) {
+				if (DeathCounter.ranking.isEmpty()) player.sendStatusMessage(new TextComponentTranslation("dc.command.noDeaths"), false);
+				else {
 					boolean isPlayer = false;
 					player.sendStatusMessage(new TextComponentTranslation("dc.command.leaderboard"), false);
-					for(int i = 1; i <= DeathCounter.leaderboardCount && i - 1 < DeathCounter.ranking.size(); i++)
-					{
+					for (int i = 1; i <= DeathCounter.leaderboardCount && i - 1 < DeathCounter.ranking.size(); i++) {
 						String playerName = DeathCounter.ranking.get(i - 1);
-						if(playerName.equals(player.getName()))
-						{
-							isPlayer = true;
-						}
+						if (playerName.equals(player.getName())) isPlayer = true;
+
 						int rank = DeathCounter.instance.getDisplayedRank(playerName);
 						player.sendStatusMessage(new TextComponentString((rank == 1 ? "\u00A7e" : rank == 2 ? "\u00A77" : rank == 3 ? "\u00A74" : "") + (playerName.equals(player.getName()) ? " ->" : "    ") + rank + "  " + DeathCounter.ranking.get(i - 1) + " (" + DeathCounter.instance.getDeathCount(DeathCounter.ranking.get(i - 1)) + (DeathCounter.instance.getDeathCount(DeathCounter.ranking.get(i - 1)) == 1 ? " Death)" : " Deaths)")), false);
 					}
-					if(!isPlayer)
-					{
+					
+					if (!isPlayer) {
 						int rank = DeathCounter.instance.getDisplayedRank(player.getName());
 						player.sendStatusMessage(new TextComponentString(" ->" + rank + (rank >= 10 ? " " : "  ") + player.getName() + " (" + DeathCounter.instance.getDeathCount(player.getName()) + (DeathCounter.instance.getDeathCount(player.getName()) == 1 ? " Death)" : " Deaths)")), false);
 					}
@@ -167,19 +102,15 @@ public class CommandDeathCounter extends CommandBase
 			}
 		}
 	}
-
+	
 	@Override
-	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos pos)
-	{
-		if(args.length == 1)
-		{
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos pos) {
+		if (args.length == 1) {
 			return getListOfStringsMatchingLastWord(args, "reset", "leaderboard");
-		}
-		else if (args.length == 2)
-		{
+		} else if (args.length == 2) {
 			return getListOfStringsMatchingLastWord(args, FMLCommonHandler.instance().getMinecraftServerInstance().getOnlinePlayerNames());
 		}
 		return null;
 	}
-
+	
 }
--- src/main/java/me/ichun/mods/deathcounter/common/core/DeathCounter.java
+++ src/main/java/me/ichun/mods/deathcounter/common/core/DeathCounter.java
@@ -31,270 +31,198 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-@Mod(modid = "deathcounter", name="DeathCounter",
-		version = DeathCounter.version,
-		acceptableRemoteVersions = "*",
-        dependencies = "required-after:forge@[13.19.0.2141,)",
-        acceptedMinecraftVersions = "[1.12,1.13)"
-)
-public class DeathCounter
-{
-
+@Mod(modid = "deathcounter", name = "DeathCounter", version = DeathCounter.version, acceptableRemoteVersions = "*", dependencies = "required-after:forge@[13.19.0.2141,)", acceptedMinecraftVersions = "[1.12,1.13)")
+public class DeathCounter {
+	
 	public static final String version = "1.0.0";
 	public static File saveDir;
-
+	
 	public static HashMap<String, Integer> deathCounter = new HashMap<>();
-
+	
 	public static ArrayList<String> ranking = new ArrayList<>();
-
+	
 	public static int message;
 	public static int leaderboardCount;
 	public static int outputToTextFile;
 	public static int singleSession;
-
+	
 	@Instance("deathcounter")
 	public static DeathCounter instance;
-
+	
 	@EventHandler
-	public void preInit(FMLPreInitializationEvent event)
-	{
+	public void preInit(FMLPreInitializationEvent event) {
 		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 		config.load();
-
+		
 		message = config.getInt("message", "deathcounter", 2, 0, 2, "Death Count Messages?\n0 = Disable\n1 = Short message\n2 = Long message");
 		leaderboardCount = config.getInt("leaderboardCount", "deathcounter", 5, 0, 20, "Number of names to show in the leaderboards");
 		singleSession = config.getInt("singleSession", "deathcounter", 0, 0, 1, "Do not save deaths in save folder?\n0 = No\n1 = Yes");
 		outputToTextFile = config.getInt("outputToTextFile", "deathcounter", 1, 0, 1, "Output deaths to text file in save folder?\n(This is overridden to 0 by the singleSession config)\n0 = No\n1 = Yes");
-
-		if(config.hasChanged())
-		{
-			config.save();
-		}
-
+		
+		if (config.hasChanged()) config.save();
 		MinecraftForge.EVENT_BUS.register(this);
 	}
-
+	
 	@EventHandler
-	public void serverStarting(FMLServerStartingEvent event)
-	{
+	public void serverStarting(FMLServerStartingEvent event) {
 		ICommandManager manager = event.getServer().getCommandManager();
-		if(manager instanceof CommandHandler)
-		{
-			CommandHandler handler = (CommandHandler)manager;
+		if (manager instanceof CommandHandler) {
+			CommandHandler handler = (CommandHandler) manager;
 			handler.registerCommand(new CommandDeathCounter());
 		}
 	}
-
+	
 	@EventHandler
-	public void serverStarted(FMLServerStartedEvent event)
-	{
+	public void serverStarted(FMLServerStartedEvent event) {
 		deathCounter.clear();
 		ranking.clear();
-
+		
 		loadDeaths(DimensionManager.getWorld(0));
 		sortRanking();
 	}
-
-
-	public void loadDeaths(WorldServer world)
-	{
+	
+	public void loadDeaths(WorldServer world) {
 		File dir = new File(world.getChunkSaveLocation(), "deathCounter");
-		if(!dir.exists())
-		{
-			dir.mkdirs();
-		}
+		if (!dir.exists()) dir.mkdirs();
+		
 		saveDir = dir;
 		File[] files = dir.listFiles();
-		for(File file : files)
-		{
-			if(file.getName().endsWith(".dat"))
-			{
+		for (File file : files) {
+			if (file.getName().endsWith(".dat")) {
 				String user = file.getName().substring(0, file.getName().length() - 4);
-				try
-				{
+				try {
 					NBTTagCompound tag = CompressedStreamTools.readCompressed(new FileInputStream(file));
 					deathCounter.put(user, tag.getInteger("deaths"));
-				}
-				catch(EOFException e)
-				{
+				} catch (EOFException e) {
 					console("File for " + user + " is corrupted. Flushing.", Level.WARN);
-				}
-				catch(IOException e)
-				{
+				} catch (IOException e) {
 					console("Failed to read file for " + user + ". Flushing.", Level.WARN);
 				}
 			}
 		}
 	}
-
-	public void sortRanking()
-	{
-		for(Map.Entry<String, Integer> e : deathCounter.entrySet())
-		{
+	
+	public void sortRanking() {
+		for (Map.Entry<String, Integer> e : deathCounter.entrySet()) {
 			ranking.remove(e.getKey());
-			if(e.getValue() > 0)
-			{
-				for(int i = 0; i < ranking.size(); i++)
-				{
-					if(getDeathCount(ranking.get(i)) <= e.getValue())
-					{
+			if (e.getValue() > 0) {
+				for (int i = 0; i < ranking.size(); i++) {
+					if (getDeathCount(ranking.get(i)) <= e.getValue()) {
 						ranking.add(i, e.getKey());
 						break;
 					}
 				}
-				if(!ranking.contains(e.getKey()))
-				{
-					ranking.add(e.getKey());
-				}
+				if (!ranking.contains(e.getKey())) ranking.add(e.getKey());
 			}
 		}
-		if(singleSession != 1 && outputToTextFile == 1)
-		{
+		
+		if (singleSession != 1 && outputToTextFile == 1) {
 			Properties s = new Properties();
 			File text = new File(saveDir, "deaths.txt");
-
-			if(text == null || text.isDirectory())
-			{
-				return;
-			}
-
-			for(Map.Entry<String, Integer> e : deathCounter.entrySet())
-			{
+			
+			if (text == null || text.isDirectory()) return;
+			
+			for (Map.Entry<String, Integer> e : deathCounter.entrySet()) {
 				s.setProperty(e.getKey(), Integer.toString(e.getValue()));
 			}
-
-			try(FileOutputStream fos = new FileOutputStream(text))
-			{
-				if(!text.exists())
-					text.createNewFile();
+			
+			try (FileOutputStream fos = new FileOutputStream(text)) {
+				if (!text.exists()) text.createNewFile();
 				s.store(fos, null);
-			}
-			catch(IOException e)
-			{
+			} catch (IOException e) {
 				console("Error writing deaths.txt", Level.WARN);
 			}
 		}
 	}
-
-	public void addDeath(EntityPlayer player)
-	{
+	
+	public void addDeath(EntityPlayer player) {
 		File file = new File(saveDir, player.getName() + ".dat");
-		NBTTagCompound tag = new NBTTagCompound();;
+		NBTTagCompound tag = new NBTTagCompound();
+
 		int deaths = getDeathCount(player.getName()) + 1;
 		tag.setInteger("deaths", deaths);
-
+		
 		deathCounter.put(player.getName(), deaths);
 		sortRanking();
-
-		try(FileOutputStream fos = new FileOutputStream(file))
-		{
+		
+		try (FileOutputStream fos = new FileOutputStream(file)) {
 			CompressedStreamTools.writeCompressed(tag, fos);
-		}
-		catch(IOException ioexception)
-		{
+		} catch (IOException ioexception) {
 			console("Failed to save death count for " + player.getName(), Level.WARN);
 		}
-
+		
 	}
-
-	public boolean clearDeath(String s)
-	{
-		if(s == null)
-		{
+	
+	public boolean clearDeath(String s) {
+		if (s == null) {
 			File[] files = saveDir.listFiles();
-			for(File file : files)
-			{
+			for (File file : files) {
 				file.delete();
 			}
 			deathCounter.clear();
 			ranking.clear();
 			return true;
-		}
-		else
-		{
+		} else {
 			File file = new File(saveDir, s + ".dat");
-			if(file.exists())
-			{
+			if (file.exists()) {
 				file.delete();
 				deathCounter.remove(s);
 				sortRanking();
 				return true;
-			}
-			else
-			{
-				return false;
-			}
+			} else return false;
 		}
 	}
-
-	public int getDeathCount(String s)
-	{
-		try
-		{
+	
+	public int getDeathCount(String s) {
+		try {
 			return deathCounter.get(s);
-		}
-		catch(NullPointerException e)
-		{
+		} catch (NullPointerException e) {
 			return 0;
 		}
 	}
-
-	public int getDisplayedRank(String s)
-	{
-		if(ranking.contains(s))
-		{
-			for(int i = 0; i < ranking.size(); i++)
-			{
-				if(ranking.get(i).equals(s))
-				{
+	
+	public int getDisplayedRank(String s) {
+		if (ranking.contains(s)) {
+			for (int i = 0; i < ranking.size(); i++) {
+				if (ranking.get(i).equals(s)) {
 					int rank = i;
 					int deaths = getDeathCount(s);
-					while(i > 0 && getDeathCount(ranking.get(--i)) == deaths)
-					{
-						rank--;
-					}
+					while (i > 0 && getDeathCount(ranking.get(--i)) == deaths) rank--;
 					return rank + 1;
 				}
 			}
 		}
 		return ranking.size() + 1;
 	}
-
+	
 	@SubscribeEvent(priority = EventPriority.LOWEST)
-	public void onDeath(LivingDeathEvent event)
-	{
-		if(event.getEntityLiving() instanceof EntityPlayer && FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER && FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(event.getEntityLiving().getName()) != null)
-		{
-			EntityPlayer player = (EntityPlayer)event.getEntityLiving();
+	public void onDeath(LivingDeathEvent event) {
+		if (event.getEntityLiving() instanceof EntityPlayer
+				&& FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER
+				&& FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(event.getEntityLiving().getName()) != null) {
+			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
 			addDeath(player);
-			if(message > 0)
-			{
-				if(message == 1)
-				{
-					player.sendStatusMessage(new TextComponentTranslation("dc.message.deathAndRank", getDeathCount(player.getName()), getDisplayedRank(player.getName())), false);
-				}
-				if(message == 2)
-				{
+			if (message > 0) {
+				if (message == 1) player.sendStatusMessage(new TextComponentTranslation("dc.message.deathAndRank", getDeathCount(player.getName()), getDisplayedRank(player.getName())), false);
+				
+				if (message == 2) {
 					player.sendStatusMessage(new TextComponentTranslation("dc.message.death", getDeathCount(player.getName())), false);
 					player.sendStatusMessage(new TextComponentTranslation("dc.message.rank", getDisplayedRank(player.getName())), false);
 				}
 			}
 		}
 	}
-
+	
 	@SubscribeEvent
-	public void onChatEvent(ServerChatEvent event)
-	{
-		if(event.getMessage().toLowerCase().toLowerCase().startsWith("!dc") || event.getMessage().toLowerCase().toLowerCase().startsWith("!deathcounter"))
-		{
+	public void onChatEvent(ServerChatEvent event) {
+		if (event.getMessage().toLowerCase().toLowerCase().startsWith("!dc") || event.getMessage().toLowerCase().toLowerCase().startsWith("!deathcounter")) {
 			CommandDeathCounter.broadcastLeaderboard(event.getUsername());
 			event.setCanceled(true);
 		}
 	}
-
+	
 	@SuppressWarnings("deprecation")
-	public static void console(String s, Level logLevel)
-	{
+	public static void console(String s, Level logLevel) {
 		FMLLog.log("DeathCounter", logLevel, "%s", s);
 	}
 }
